670f8637638a0a815acb73440f83e655d3c7bb0b
Victor Nascimento Bakke <gipphe@gmail.com>
2016-10-13 14:50:18 +0200

    Finalized changeLang function

---
+++ js/index.js
@@ -0,0 +1,9 @@
+(function() {
+	window.changeLang = function(lang) {
+		if (typeof lang !== 'string' || lang.length !== 2) {
+			return;
+		}
+		var body = document.body;
+		body.setAttribute('lang', lang);
+	};
+}());
