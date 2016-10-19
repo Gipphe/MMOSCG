4b7eb56b97acb9c0e8daf60a0795b519dc2324a3
Victor Nascimento Bakke <gipphe@gmail.com>
2016-10-19 15:48:43 +0200

    Added responsive rules to navigation and images. Finalized markup. Added .selected class moving to js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,26 @@
 (function() {
+	document.getElementById('no').addEventListener('click', function(){
+		this.classList.add('selected');	
+		en.classList.remove('selected');
+		ar.classList.remove('selected');
+	});
+
+	document.getElementById('ar').addEventListener('click', function() {
+		this.classList.add('selected');
+		no.classList.remove('selected');
+		en.classList.remove('selected');
+	});
+
+	document.getElementById('en').addEventListener('click', function() {
+		this.classList.add('selected');
+		no.classList.remove('selected');
+		ar.classList.remove('selected');
+	});
 	window.changeLang = function(lang) {
 		if (typeof lang !== 'string' || lang.length !== 2) {
 			return;
 		}
-		var body = document.body;
-		body.setAttribute('lang', lang);
-
-		// var selected = document.getElementById('selected');
-
-
+		var html = document.documentElement;
+		html.setAttribute('lang', lang);
 	};
 }());
